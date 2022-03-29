fa484bf0e8b64ed50c0570c682f02f479c270b41
Gabriel SCHWAB <gabriel.schwab@utbm.fr>
2022-03-29 16:03:20 +0200

    manque fin exo 2 et exo 3

--- tp3/STM32F401/Src/main.c
+++ tp3/STM32F401/Src/main.c
@@ -32,111 +32,12 @@ static void pin_init(void);
 
 int main(void)
 {
-	led_init();
-
-	TIM_TypeDef* tim3 = TIM3;
-	GPIO_TypeDef * PA = GPIOA, *PC = GPIOC;
-	int is_activate = 1;
 
 
-
-	//CK_NT * PSC = CK_CNT => PSC= CK_CNT / CK_INIT = 10/16000
-	tim3->PSC = (16-1/1)*1000;
-	//auto reload = T_timer/(PSC*CK_INT) - 1 = 1/2*10000 -1
-	tim3->ARR = 1/(2*10000) -1;
-	//set EGR register bit to 1 to force the update and start the count at 0
-	tim3->EGR |= TIM_EGR_UG_Msk;
-	//set bit OGR from CR1 register of TIM3 to 1 for 'one shot' behaviour
-	//TIM3->CCR1 |= TIM_CR1_OPM_Msk;
-	//errase status register (p347 'reset value = 0x0000')
-	tim3->SR &= 0x0000;
-	//start timer
-	TIM3->CR1 |= TIM_CR1_CEN_Msk;
-
-
-
-	for(;;){
-
-		/* activation alternate fonction pour GPIOB pour la pin PB7*/
-		GPIOB->MODER &= ~GPIO_MODER_MODER7_Msk;// mise à '10' des bits du registre moder7 pour alternate fonction
-		GPIOB->MODER |= GPIO_MODER_MODER7_1;
-		GPIOB->AFR[0] &= (0xF<<1*4);// dans la data sheet, section alternate fonction mapping p44, on voit que TIM4_CH2 est mappé avec la fonction alternative AF02 sur la pin PA1
-		GPIOB->AFR[0] |= (2<<1*4);
-
-		/* désactivation input caputre mode */
-		TIM4->CCER &= ~TIM_CCER_CC1E_Msk;
-		TIM4->CCER &= ~TIM_CCER_CC2E_Msk;
-
-		/* modification de CCMRR1 */
-		TIM4->CCMR1
-
-
-
-
-
-
-
-
-
-//		/** clignotage de led**/
-//		while(!(tim3->SR & ~TIM_SR_UIF_Msk)){
-//		}
-//		if(is_activate){
-//			/* éteint la led */
-//			PA->ODR = PA->ODR & ~(1<<5);// met le bit 5 à 0
-//			//PA->ODR &= ~GPIO_ODR_OD5;
-//		}
-//		else{
-//			/* allume la led */
-//			PA->ODR = PA->ODR | 1<<5; // met le bit 5 à 1
-//			//PA->ODR |= GPIO_ODR_OD5;	//(1<<5);
-//		}
-//
-//		tim3->SR &= ~TIM_SR_UIF_Msk;
-	}
-	return 0;
-}
-//alternate fonction 2 au tim4 ch2 (ti2)
-//pb7 c'est GPIOB
-
-
-
-void led_init()
-{
-	/* Activation horloge */
-		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
-
-		/* Configuration du GPIOA */
-		GPIO_TypeDef *PA = GPIOA;
-		GPIO_TypeDef *PC = GPIOC;
-		PA->MODER = PA->MODER & ~(1<<11); // met le bit 11 à 0
-		PA->MODER = PA->MODER | (1<<10); // met le bit 10 à 1
-
-		/* allume la led */
-		PA->ODR = PA->ODR | 1<<5; // met le bit 5 à 1
-
-		/* éteint la led */
-		PA->ODR = PA->ODR & ~(1<<5);// met le bit 5 à 0
-
-		// Pour la config des registres voir page 151 du reference-manual
-
-		/* Met PA0 en input et PA1 en general purpose output mode = ...0010 */
-		PA->MODER &= ~(1<<0);
-		PA->MODER &= ~(1<<1);
-		PA->MODER &= ~(1<<3);
-		PA->MODER |= 0b0100;
-
-		/* Mettre PA1 en open-drain = ...0. */
-		PA->OTYPER &= ~(1<<1);
-
-		/* Mettre PA0 en pull-up = ...01 */
-		PA->OSPEEDR &= ~(1<<1);
-		PA->OSPEEDR |= 1<<0;
-}
-
-static void pin_init(void)
-{
-	GPIO_TypeDef * PA = GPIOA, *PC = GPIOC;
+	/* 2 port instance are declared and mapped on GPIOA and GPIOC address
+		 * GPIOA and GPIOC are constant address defined in stm32f401xe.h
+		 */
+		GPIO_TypeDef * PA = GPIOA, *PC = GPIOC, *PB = GPIOB;
 
 		/* modify bit 0 and 3 of RCC AHB1ENR register to enable the clock
 		 * of GPIOA and GPOC
@@ -144,7 +45,7 @@ static void pin_init(void)
 		 * RCC_AHB1ENR_GPIOCEN RCC_AHB1ENR_GPIOCEN are constant also defined in
 		 * stm32f401xe.h
 		 */
-		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN;
 
 		/* GPIOA configuration :  output, slow speed, push-pull, no pull
 		 * After reset, only the output mode should be set. the other configuration
@@ -152,10 +53,10 @@ static void pin_init(void)
 		 */
 		/* PA5 in output mode */
 		PA->MODER &= ~GPIO_MODER_MODER5_Msk; /* GPIO_MODER_MODER5_Msk : constante
-												définie dans stm32f401xe.h */
+												dÃ©finie dans stm32f401xe.h */
 		PA->MODER |= GPIO_MODER_MODER5_0;	/* GPIO_MODER_MODER5_0, constante
-												définie dans stm32f401xe.h */
-		/* Si on considère que PA est dans un état indéterminé */
+												dÃ©finie dans stm32f401xe.h */
+		/* Si on considÃ¨re que PA est dans un Ã©tat indÃ©terminÃ© */
 		/* output push pull */
 		PA->OTYPER &= ~GPIO_OTYPER_OT5_Msk;			// clear bit field
 		/* slow speed, better for low electrical interference (less noisy) */
@@ -166,10 +67,145 @@ static void pin_init(void)
 		/* configure PC13 : input, no pull */
 		PC->MODER &= ~GPIO_MODER_MODE13_Msk;		// clear bit field
 		PC->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;		// clear bit field
+		/* other config registers are irrelevant for pins in input mode */
 
+		/* Activer horloger GPIOA */
+
+		/* PA2(TX) en alternate function nÂ°7 */
+		PB->AFR[0] &= ~GPIO_AFRL_AFRL7;
+		PB->AFR[0] |= GPIO_AFRL_AFRL7_1;
+		/* alternate function PA7*/
+		PB->MODER &= ~GPIO_MODER_MODER7;
+		PB->MODER |= GPIO_MODER_MODER7_1;
+
+
+
+		TIM_TypeDef* tim3 = TIM3;
+		int is_activate = 0;
+
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+
+		//CK_CNT * PSC = CK_PSC => PSC= CK_PSC / CK_CNT = 16 000/10
+		TIM3->PSC = SystemCoreClock /10000 -1;
+		//auto reload = CK_PSC / f_led = 10000/2 - 1
+		TIM3->ARR = 10000/10/2 - 1;
+		//set EGR register bit to 1 to force the update and start the count at 0
+		TIM3->EGR &= ~TIM_EGR_UG;
+		TIM3->EGR |= TIM_EGR_UG_Msk;
+		//set bit OGR from CR1 register of TIM3 to 1 for 'one shot' behaviour
+		//TIM3->CCR1 |= TIM_CR1_OPM_Msk;
+		//errase status register (p347 'reset value = 0x0000')
+		TIM3->SR = 0;
+		//start timer
+		TIM3->CR1 |= TIM_CR1_CEN_Msk;
+
+
+//----------------------------------------------------------------------partie 1 clignotement led---------------------------------------------
+//	for(;;){
+//
+//		/** clignotage de led**/
+//		if((TIM3->SR & TIM_SR_UIF_Msk)){
+//
+//			if(is_activate){
+//				/* éteint la led */
+//				PA->ODR &= ~GPIO_ODR_OD5;
+//				is_activate =0;
+//			}
+//			else{
+//				/* allume la led */
+//				PA->ODR |= GPIO_ODR_OD5;	//(1<<5);
+//				is_activate = 1;
+//			}
+//
+//			tim3->SR &= ~TIM_SR_UIF_Msk;
+//		}
+//	}
+		//----------------------------------------------------------------------partie 2 bouton timer---------------------------------------------
+
+
+
+		/* activation alternate fonction pour GPIOB pour la pin PB7*/
+		GPIOB->MODER &= ~GPIO_MODER_MODER7_Msk;// mise à '10' des bits du registre moder7 pour alternate fonction
+		GPIOB->MODER |= GPIO_MODER_MODER7_1;
+		GPIOB->AFR[0] &= GPIO_AFRL_AFSEL7_Msk;// dans la data sheet, section alternate fonction mapping p44, on voit que TIM4_CH2 est mappé avec la fonction alternative AF02 sur la pin PA1
+		GPIOB->AFR[0] |= GPIO_AFRL_AFSEL7_1;
+
+		/* désactivation input capture mode */
+		TIM4->CCER &= ~TIM_CCER_CC1E_Msk;
+		TIM4->CCER &= ~TIM_CCER_CC2E_Msk;
+
+		/* modification de CCMRR1 */
+		TIM4->CCMR1 &= ~TIM_CCMR1_IC2F_Msk;
+		TIM4->CCMR1 |= TIM_CCMR1_IC2F_3; //valeur du filtre pour les rebonds
+
+		/* configure capture prescaler, on met à 0 car on veut capturer tous les évènements */
+		TIM4->CCMR1 &= ~TIM_CCMR1_IC1PSC_Msk;
+		TIM4->CCMR1 &= ~TIM_CCMR1_IC2PSC_Msk;
+
+		/**/
+		TIM4->CCMR1 &= ~TIM_CCMR1_CC1S_Msk;
+		TIM4->CCMR1 |= TIM_CCMR1_CC1S_1; //met le registre à '10' pour mapper IC1 à TI2
+
+		/**/
+		//TIM4->CCER;
+
+
+
+
+
+
+
+
+
+
+
+
+		return 0;
+}
+
+void TIM32_init_pwm(void){
+
+	/* horloge du TIM2 */
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+
+	PB->AFR[0] &= ~GPIO_AFRL_AFSEL5_Msk;
+	PB->AFR[0] |= GPIO_AFRL_AFRL5_1;
+	/* alternate function PA7*/
+	PB->MODER &= ~GPIO_MODER_MODER5;
+	PB->MODER |= GPIO_MODER_MODER5_1;
+
+	//On veut une granularité de 1000 c'est à dire ARR = 1000, et une fréquence de sortie de 100Hz, donc f en sortie de prescaler = 1000*100 = 10e5
+	//donc on règle le prescaler à 160
+
+
+	//CK_CNT * PSC = CK_PSC => PSC= CK_PSC / CK_CNT = 16 000/10
+	TIM2->PSC = SystemCoreClock /100000 -1;
+	//auto reload = CK_PSC / f_led = 10000/2 - 1
+	TIM2->ARR = 1000 - 1;
+	//set EGR register bit to 1 to force the update and start the count at 0
+	TIM2->EGR &= ~TIM_EGR_UG;
+	TIM2->EGR |= TIM_EGR_UG_Msk;
+	TIM2->SR = 0;
+
+	//mise en output compare mode
+	TIM2->CCMR1 &= ~TIM_CCMR1_OC1M_Msk;//PWM mode 1 : set OC1M bits from register CCMR1 to '110' (p351 ref manual)
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_1;
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2;
+
+	TIM2->CCER |= TIM_CCER_CC1E_Msk; //set CC1E bits from CCER register to enable output
+	/// il y a des trucs à rajouter pour ccer, voir doc
+
+	TIM2->CCR1 = 10;//jsplus pk
+
+	//start timer
+	TIM2->CR1 |= TIM_CR1_CEN_Msk;
 }
 
 
+void TIM2_set_pwn(unint32_t duty)
+{
+
+}
 
 
 
